62fa1edb06d329b55ef90a1edb50704b8de3914a
我妻　伶治 <otete1996@yahoo.co.jp>
2018-04-30 16:38:11 +0900

    destroyigai

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,23 +1,53 @@
 class BooksController < ApplicationController
+  def index
+    @books = Book.all
+  end
 
-def index
-end
+  def show
+    @book = Book.find(params[:id])
+  end
 
-def new
-end
-
-def create
-end
-
-def edit
-end
-
-def update
-end
-
-def destroy
-end
+  def new
+    @book = Book.new
+  end
 
 
- 
+  def create
+    ### ここから追記
+    @book = Book.new(book_params)
+    # 上の行はインスタンスを作っただけ。saveメソッドで保存しないと、消える。
+    # モデルのデータをデータベースに反映
+    @book.save
+
+    # 処理の後はindex画面を表示する
+    redirect_to books_path
+    ### ここまで
+  end
+  def update
+    @book=Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to(book_path(@book))
+  end
+  def edit
+    @book=Book.find(params[:id])
+  end
+
+  def destroy
+    
+  end
+
+
+  ### ここから追記
+  private
+
+  # params.require(key).permit(filter)
+  # key
+  #   Strong Parameters を適用したい params の key を指定する。
+  # filter
+  #   Strong Parameters で許可するカラムを指定する。
+
+  def book_params
+    params.require(:book).permit(:name, :price, :publish, :publish_date)
+  end
+  ### ここまで
 end
