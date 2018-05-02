942986db51632039be3ec8001c960d99f95d34a3
我妻　伶治 <otete1996@yahoo.co.jp>
2018-05-02 11:15:49 +0900

    レビュー登録機能追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,5 @@
 class Book < ApplicationRecord
+  has_many :reviews
   validates :name, presence: true, length:{maximum: 20}
   validates :publish, presence: true
   validates :price, presence: true
